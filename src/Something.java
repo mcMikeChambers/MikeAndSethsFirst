cd2ea6d6be02f8b5a9947237da832a422a473f6f
HARMONSC21 <harmonsc21@gcc.edu>
2023-03-01 13:55:28 -0500

    this is a test

--- src/Something.java
+++ src/Something.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Something {
     public static void main(String[] args) {
         System.out.println("Hey seth");
+        Scanner in = new Scanner(System.in);
+        System.out.println("How are you?");
+        String answer = in.next();
     }
 }
